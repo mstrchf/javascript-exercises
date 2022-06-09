b9eb445b902cc5927f4f5445a74a336b11e98141
pho <phonum5@gmail.com>
2022-06-09 14:38:16 +0000

    at task 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,6 @@
 const leapYears = function (year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0 && year % 400 === 0) {
-      return true;
-    }
-
-    return true;
-  } else return false;
+  if (year % 4 === 0 && year % 100 !== 0) return true;
+  return year % 400 === 0;
 };
 
 // Do not edit below this line
