5c02bdfc795cb18b1d401913c165cb47a10bd4bc
pho <phonum5@gmail.com>
2022-07-06 15:04:56 +0000

    at challenge 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let total = 0
+	nums.forEach(num => {
+    total += num;
+  });
+
+  return total
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let total = 1;
+  nums.forEach(num => {
+    total *= num
+  });
 
+  return total
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  
+  let factor = 1
+	for (let i = 1; i <= num; i++) {
+    factor *= i
+  }
+
+  return factor
 };
 
 // Do not edit below this line
