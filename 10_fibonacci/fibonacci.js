b476873279521647299a1268cc0ef04ffa24ae33
pho <phonum5@gmail.com>
2022-07-06 16:26:57 +0000

    at challenge 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  let n1 = 0;
+  let n2 = 1;
+  let sum = 0;
 
+  for (let i = 2; i <= num; i++) {
+    sum = n1 + n2;
+    n1 = n2;
+    n2 = sum;
+  }
+
+  return num == 0 ? n1 : n2;
 };
 
 // Do not edit below this line
