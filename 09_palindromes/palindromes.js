2ccc4872b8240303903d203362f853d62a44e83f
pho <phonum5@gmail.com>
2022-07-06 16:12:26 +0000

    at challenge 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let splittedText = text.split('') 
+    let normalText = removeNonText(splittedText).join("")
+    let reversedText = removeNonText(splittedText).reverse().join("")
+
+    console.log(normalText);
+    console.log(reversedText);
+
+    return normalText == reversedText
 
 };
+
+const removeNonText = function (arr) {
+    let sanitizedText = []
+    arr.forEach(char => {
+        if (isLetter(char)) {
+            sanitizedText.push(char.toLowerCase())
+        }
+    });
+
+    return sanitizedText
+}
+
+function isLetter(letter) {
+    return letter.toUpperCase() != letter.toLowerCase()
+}
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
